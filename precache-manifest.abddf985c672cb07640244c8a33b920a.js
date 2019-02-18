e914b0bd018c5a259b84236bf4bcb56162be2ec7
Alexey Lebed <lebeddos@gmail.com>
2019-02-17 20:27:38 -0800

    Updates

---
+++ precache-manifest.abddf985c672cb07640244c8a33b920a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "d53b1226a378c433413c",
+    "url": "/clickygame/static/js/main.d53b1226.chunk.js"
+  },
+  {
+    "revision": "efcf094c0407b87d0978",
+    "url": "/clickygame/static/js/2.efcf094c.chunk.js"
+  },
+  {
+    "revision": "d53b1226a378c433413c",
+    "url": "/clickygame/static/css/main.3e2eb47f.chunk.css"
+  },
+  {
+    "revision": "bba0c0d0e3c80e81c9c44dc6b17eb58c",
+    "url": "/clickygame/index.html"
+  }
+];
